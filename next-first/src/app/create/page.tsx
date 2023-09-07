61d323ef034c6e63e019526f67260982a6880cd1
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-07 22:47:50 +0900

    글 생성

--- next-first/src/app/create/page.tsx
+++ next-first/src/app/create/page.tsx
@@ -1,8 +1,48 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+interface IResult {
+  title: string,
+  id: number
+}
 export default function Create() {
-  
+  const router = useRouter()
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title:content})
+    }
+    fetch(`http://localhost:9999/topics`, options)
+    .then((res) => res.json())
+    .then((res: IResult) => 
+    {
+      const id = res.id
+      console.log(id)
+      router.push(`/read/${id}`)
+    }
+    )
+  }
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, setFuc: (params: string) => void) => {
+    setFuc(e.target.value)
+  }
   return (
-    <div>
-      Create
-    </div>
+    <form onSubmit={handleSubmit}>
+      <p>
+        <input type="text" value={title} onChange={(e) => (handleChange(e, setTitle))} placeholder="title"/>
+      </p>
+      <p>
+        <textarea onChange={(e) => (handleChange(e, setContent))} value={content} name="body" id="body" ></textarea>
+      </p>
+      <p>
+        <input type="submit" value={'create'} />
+      </p>
+    </form>
   )
 }
