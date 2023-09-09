4a82b09a11bb9faa4d07d45adb13f698f44808d9
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-10 00:44:28 +0900

    TMDB 리스팅 가져오기

--- nomad-nextjs/components/NavBar.tsx
+++ nomad-nextjs/components/NavBar.tsx
@@ -1,24 +1,46 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css'
-export default function NavBar(){
-  const router = useRouter()
-  const pathName = router.pathname
-  console.log(pathName)
+
+export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/" className={pathName === '/' ? styles.active : ''}>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
           Home
-      </Link>
-      <Link href="about" className={pathName === '/about' ? styles.active : ''}>about</Link>
-      <style jsx>
-        {`
-          nav{
-            display: flex;
-            gap: 15px;
-          }
-        `}
-      </style>
+        </Link>
+        <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>
+          About
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
-  )
+  );
 }
--- nomad-nextjs/pages/index.tsx
+++ nomad-nextjs/pages/index.tsx
@@ -3,14 +3,74 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Seo from '../components/Seo'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+interface IMovie {
+  id: number,
+  title: string,
+  poster_path: string
+}
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  const [movies, setMovies] = useState<IMovie[]>([])
+  const onClick = (id: number, title: string) => {
+    router.push(`/movies/${title}/${id}`);
+  };
+  useEffect(() => {
+        fetch('/api/movies')
+        .then(res => res.json())
+        .then(res => {
+          console.log(res.results)
+          setMovies(res.results)
+        })
+    
+  },[])
   return (
     <>
       <Seo title='Home'/>
-      <h1>
-        home
-      </h1>
+      {
+        movies.map((movie) => (
+          <div
+          onClick={() => onClick(movie.id, movie.title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.title}/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </h4>
+        </div>
+        ))
+      }
+          <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </>
   )
 }
