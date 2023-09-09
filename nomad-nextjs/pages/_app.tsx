98a04b4cb5e4549ec66cabfc8509fc356a6be4f4
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-09 22:04:51 +0900

    global style 적용

--- nomad-nextjs/pages/_app.tsx
+++ nomad-nextjs/pages/_app.tsx
@@ -1,8 +1,22 @@
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+
+      <style global jsx>
+        {`
+          h1{
+            font-size: 24px;
+          }
+        `}
+      </style>
+    </>
+  )
 }
 
 export default MyApp
--- nomad-nextjs/pages/index.tsx
+++ nomad-nextjs/pages/index.tsx
@@ -2,16 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <NavBar />
+    <>
       <h1>
         home
       </h1>
-    </div>
+    </>
   )
 }
 
