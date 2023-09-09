b458d7b3a7cb66697c0186844a7404753062cf73
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-09 23:20:26 +0900

    rediect 설정

--- nomad-nextjs/next.config.js
+++ nomad-nextjs/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   env: {
     NEXT_PUBIC_API_URL: process.env.NEXT_PUBIC_API_URL,
+  },
+  async redirects(){
+    return [
+      {
+        source: '/contact',
+        destination: 'https://github.com/munseunggyu',
+        permanent: false
+      }
+    ]
   }
 }
 
