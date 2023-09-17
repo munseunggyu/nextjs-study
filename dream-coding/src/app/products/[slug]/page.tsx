3c6ce9877cf7c7fea032b8023caa35fd3ba94ca9
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-17 15:54:38 +0900

    ISR 예제 추가

--- dream-coding/src/app/products/[slug]/page.tsx
+++ dream-coding/src/app/products/[slug]/page.tsx
@@ -10,7 +10,7 @@ export function generateMetadata({ params }: Props){
     title: `제품의 이름은 ${params.slug}입니다`
   }
 }
-
+export const revalidate = 3 // revalidate at most every hour
 export default async function Sulg({ params: {slug} }: Props) {
   // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그것을 보여준다.
   const product = await getProduct(slug)
--- dream-coding/src/app/products/page.tsx
+++ dream-coding/src/app/products/page.tsx
@@ -4,10 +4,8 @@ import Counter from '../components/Counter'
 import { getProducts } from '../service/products'
 
 export default async function ProductsPage() {
-  console.log('서버')
   // 서버 파일 (데이터베이스)에 있는 제품 리스트를 읽어와서, 그것을 보여준다
   const products = await getProducts()
-  console.log(products)
   return (
     <div>
       <h2>제품 소개 페이지</h2>
