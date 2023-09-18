08d02d5bf4de44a16a769cd004fcb0fdcbe252d7
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-09-18 22:09:03 +0900

    nextjs image 예제 추가

--- dream-coding/src/app/products/page.tsx
+++ dream-coding/src/app/products/page.tsx
@@ -5,19 +5,29 @@ import Counter from '../components/Counter'
 import { getProducts } from '../service/products'
 import axios from 'axios'
 import MeowArticle from '../components/MeowArticle'
+import Image from 'next/image'
+// import cloth from '/public/next.svg'
+import cloth from '/public/clothes.jpg'
+
 type IProps = {
   props: {
     data: string
   }
 }
 export default async function ProductsPage() {
-  // {props: {data}}: IProps
   // 서버 파일 (데이터베이스)에 있는 제품 리스트를 읽어와서, 그것을 보여준다
   const products = await getProducts()
   return (
     <div>
       <h2>제품 소개 페이지</h2>
       <Counter />
+      <Image src={cloth} alt='' />
+      <Image
+        src="https://s3.amazonaws.com/my-bucket/profile.png"
+        alt="Picture of the author"
+        width={400}
+        height={400}
+      />
       <ul>
         {
           products.map(val => (
